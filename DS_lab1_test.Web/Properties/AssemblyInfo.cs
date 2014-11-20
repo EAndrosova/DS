3565dcf63b045d184d070c718f87feb5812fffbe
EAndrosova <eandrosova@list.ru>
2014-11-20 20:28:21 +0400

    первая лаба

---
+++ DS_lab1_test.Web/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// общие сведения об этой сборке.
+[assembly: AssemblyTitle("DS_lab1_test.Web")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DS_lab1_test.Web")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False в параметре ComVisible делает типы в этой сборке невидимыми 
+// для COM-компонентов.  Если необходим доступ к типу в этой сборке из 
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Указанный ниже идентификатор GUID предназначен для идентификации библиотеки типов, если этот проект будет видимым для COM-объектов
+[assembly: Guid("309df290-a01b-45b5-8da4-11ebfdacff38")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
+// используя "*", как показано ниже:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ DS_lab1_test/MainPage.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace DS_lab1_test
+{
+    public partial class MainPage : UserControl
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private WebClient client;
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            client = new WebClient();
+
+            // client id and secret key form SoundCloud
+            string ClientId = "ef84c4f73eba7d7c344687eee8eed747";
+            string ClientSecret = "97e17d2011a34fa74e399f940ddb8a42";
+
+            // credentials (username & password)
+            string username = tbLogin.Text;
+            string password = pbPwd.Password;
+
+            // authentication data
+            string postData = "client_id=" + ClientId
+                + "&client_secret=" + ClientSecret
+                + "&grant_type=password&username=" + username
+                + "&password=" + password;
+
+            // authentication
+            string soundCloudTokenRes = "https://api.soundcloud.com/oauth2/token";
+            var scUri = new Uri(soundCloudTokenRes);
+            client.UploadStringCompleted += new UploadStringCompletedEventHandler(client_UploadStringCompleted);
+            client.UploadStringAsync(scUri, postData);
+
+        }
+
+        void client_UploadStringCompleted(object sender, UploadStringCompletedEventArgs e)
+        {
+            //throw new NotImplementedException();
+            if (e.Error == null)
+            {
+                lblError.Content = "Succeeded!";
+                string tokenInfo = e.Result.ToString();
+                MessageBox.Show(tokenInfo);
+                // parse access token
+                tokenInfo = tokenInfo.Remove(0, tokenInfo.IndexOf("token\":\"") + 8);
+                string token = tokenInfo.Remove(tokenInfo.IndexOf("\""));
+
+                // SoundCloud API get request
+                string soundCloudMeReq = "https://api.soundcloud.com/me";
+                //string soundCloudMeReq = "https://api.soundcloud.com/me/connections.json";
+                var scUri = new Uri(soundCloudMeReq + "?oauth_token=" + token);
+                client.DownloadStringCompleted += new DownloadStringCompletedEventHandler(client_DownloadStringCompleted);
+                client.DownloadStringAsync(scUri);
+            }
+            else
+            {
+                //lblError.Content = e.Error.ToString();
+                lblError.Content = "Wrong login or password!";
+            }
+        }
+
+        void client_DownloadStringCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+            //throw new NotImplementedException();
+            if (e.Error == null)
+            {
+                string meData = e.Result;
+                MessageBox.Show(meData);
+            }
+            else
+            {
+                //lblError.Content = e.Error.ToString();
+                lblError.Content = "Some problem with connecting to server!";
+            }
+        }
+    }
+}
